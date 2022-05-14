ec16631f2b689c94c6e94d0b8b7b7db485fbdabf
Igor Line <igor@line.ee>
2022-05-14 08:28:39 +0200

    feat(frontend): add currency input field and craco

---
+++ packages/frontend/src/components/Icons.tsx
@@ -0,0 +1,27 @@
+export const ErrorIcon = () => (
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      className="fill"
+      d="M7 14C10.866 14 14 10.866 14 7C14 3.13401 10.866 0 7 0C3.13401 0 0 3.13401 0 7C0 10.866 3.13401 14 7 14Z"
+      fill="#F58C6A"
+    />
+    <path
+      d="M7 4.20007V7.00007"
+      stroke="#FBF9F6"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M7 9.80005H7.00764"
+      stroke="#FBF9F6"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);

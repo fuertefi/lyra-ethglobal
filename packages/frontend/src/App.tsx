bec9f00921887b3922889b3d442349999244cb7b
spider-g <guilherme@sage.art>
2022-05-15 11:17:23 -0300

    remove chains from wallet connector

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -60,7 +60,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.optimism],
   [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
 );
 

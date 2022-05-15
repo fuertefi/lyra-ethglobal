7177bf16e9a6172b39dfdde6bfb21a567a45150c
spider-g <guilherme@sage.art>
2022-05-15 16:05:39 -0300

    tooltip & strategy details panel

--- packages/frontend/src/components/Strategy/StrategyDetailsPanel.tsx
+++ packages/frontend/src/components/Strategy/StrategyDetailsPanel.tsx
@@ -1,6 +1,8 @@
+import { format } from "date-fns";
 import { StrategyDetails } from "../../pages/api/strategyServices";
+import DoubleColumnPanel from "./DoubleColumnPanel";
+import SingleColumnPanel from "./SingleColumnPanel";
 import StrategyCap from "./StrategyCap";
-import StrategyPeriodAndCollateral from "./StrategyPeriodAndCollateral";
 
 interface Props {
     strategyDetails: StrategyDetails;
@@ -13,11 +15,26 @@ const StrategyDetailsPanel = ({ strategyDetails }: Props) => (
             cap={strategyDetails.capacity}
             currency={strategyDetails.currency}
         />
-        <StrategyPeriodAndCollateral
-            cycleNumber={strategyDetails.cycleNumber}
-            startTime={strategyDetails.startTime}
-            endTime={strategyDetails.endTime}
-            collateral={strategyDetails.collateralDescription}
+        <DoubleColumnPanel
+           headline1={`Current Period: ${strategyDetails.cycleNumber}`}
+           text1={`${format(strategyDetails.startTime, "MMM dd")} - ${format(strategyDetails.endTime, "MMM dd")}`}
+           tooltip1={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+           headline2={"Strategy Collateral"}
+           text2={strategyDetails.collateralDescription}
+           tooltip2={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+        />
+        <DoubleColumnPanel
+           headline1={`Current ETH Price`}
+           text1={`2500 USD`}
+           tooltip1={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+           headline2={"Another Parameter"}
+           text2={`Some value`}
+           tooltip2={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+        />
+        <SingleColumnPanel
+            headline={"Options Positions"}
+            text={strategyDetails.optionsPositions}
+            tooltip={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
         />
     </>
 );

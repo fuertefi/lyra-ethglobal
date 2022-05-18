bc3d155372a91fac332edeb678d68c6fd7986ca6
Igor Line <igor@line.ee>
2022-05-18 13:54:34 +0200

    chore(contracts): update gitignore and remove commited files

--- packages/contracts/dist/test/unit-tests/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WEEKS_PER_YEAR = exports.FEE_MULTIPLIER = void 0;
-exports.FEE_MULTIPLIER = 1e6;
-exports.WEEKS_PER_YEAR = 52142857;

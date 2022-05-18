85f0ecfb47f7685b85345e28967d04577756fa1e
Igor Line <igor@line.ee>
2022-05-19 01:08:57 +0200

    feat(contracts): export deployed contracts addresses

---
+++ packages/contracts/constants.ts
@@ -0,0 +1,15 @@
+export const NETWORKS = {
+  KOVAN_OVM: 69
+}
+
+export const DEPLOYED_CONTRACTS = {
+  HackMoneyStrategy: {
+    [NETWORKS.KOVAN_OVM]: "0x7d855253cee122b554a5f4ec4ad5f7c1a0d57131"
+  },
+  LyraVault: {
+    [NETWORKS.KOVAN_OVM]: "0xf8c3b27c5175c04839e7073a805f195bcf812840"
+  },
+  BasicFeeCounter: {
+    [NETWORKS.KOVAN_OVM]: "0xff577f0eca15a512dae5e70388224bdede6950ac"
+  }
+}

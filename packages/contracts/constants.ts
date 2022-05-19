333c1d08fc69eea0a0154bff703282a9e3dcb8b7
Igor Line <igor@line.ee>
2022-05-19 21:32:52 +0200

    feat(contracts): add basic deploy script

--- packages/contracts/constants.ts
+++ packages/contracts/constants.ts
@@ -1,15 +1,18 @@
 export const NETWORKS = {
-  KOVAN_OVM: 69
-}
+  KOVAN_OVM: 69,
+};
 
 export const DEPLOYED_CONTRACTS = {
   HackMoneyStrategy: {
-    [NETWORKS.KOVAN_OVM]: "0x7d855253cee122b554a5f4ec4ad5f7c1a0d57131"
+    [NETWORKS.KOVAN_OVM]: "0x7d855253cee122b554a5f4ec4ad5f7c1a0d57131",
   },
   LyraVault: {
-    [NETWORKS.KOVAN_OVM]: "0xf8c3b27c5175c04839e7073a805f195bcf812840"
+    [NETWORKS.KOVAN_OVM]: "0xf8c3b27c5175c04839e7073a805f195bcf812840",
   },
   BasicFeeCounter: {
-    [NETWORKS.KOVAN_OVM]: "0xff577f0eca15a512dae5e70388224bdede6950ac"
-  }
-}
+    [NETWORKS.KOVAN_OVM]: "0xff577f0eca15a512dae5e70388224bdede6950ac",
+  },
+  CurveAddress: {
+    [NETWORKS.KOVAN_OVM]: "0xcce8283dB5685b63C090fec086829E8C9F422623",
+  },
+};

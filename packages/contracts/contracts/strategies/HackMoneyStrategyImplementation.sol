b787bd1760b6462b8281835050d8b7fd09d88b71
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-07-14 21:36:35 +0100

    edit:contract

--- packages/contracts/contracts/strategies/HackMoneyStrategyImplementation.sol
+++ packages/contracts/contracts/strategies/HackMoneyStrategyImplementation.sol
@@ -8,13 +8,15 @@ import "hardhat/console.sol";
 // standard strategy interface
 import "../interfaces/IHackMoneyStrategy.sol";
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
 // Lyra
 import {VaultAdapter} from "@lyrafinance/protocol/contracts/periphery/VaultAdapter.sol";
 import {GWAVOracle} from "@lyrafinance/protocol/contracts/periphery/GWAVOracle.sol";
 
 // Libraries
 import {Vault} from "../libraries/Vault.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";`
 import {HackMoneyVault} from "../core/HackMoneyVault.sol";
 import {DecimalMath} from "@lyrafinance/protocol/contracts/synthetix/DecimalMath.sol";
 import {SignedDecimalMath} from "@lyrafinance/protocol/contracts/synthetix/SignedDecimalMath.sol";
@@ -22,25 +24,29 @@ import {SignedDecimalMath} from "@lyrafinance/protocol/contracts/synthetix/Signe
 // StrategyBase to inherit
 import {HackMoneyStrategyBase} from "./HackMoneyStrategyBase.sol";
 
-contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
-    using DecimalMath for uint;
-    using SignedDecimalMath for int;
+contract HackMoneyStrategyImplementation is
+    HackMoneyStrategyBase,
+    IHackMoneyStrategy,
+    Initializable
+{
+    using DecimalMath for uint256;
+    using SignedDecimalMath for int256;
 
     // example strategy detail
     struct HackMoneyStrategyDetail {
-        uint minTimeToExpiry;
-        uint maxTimeToExpiry;
-        int mintargetDelta; // 15%
-        int maxtargetDelta; // 85%
-        uint maxDeltaGap; // 5%
-        uint minVol; // 80%
-        uint size; // 15
+        uint256 minTimeToExpiry;
+        uint256 maxTimeToExpiry;
+        int256 mintargetDelta; // 15%
+        int256 maxtargetDelta; // 85%
+        uint256 maxDeltaGap; // 5%
+        uint256 minVol; // 80%
+        uint256 size; // 15
     }
 
     HackMoneyStrategyDetail public strategyDetail;
-    uint public activeExpiry;
-    uint public currentBoardId;
-    uint public ivLimit = 2 * 1e18;
+    uint256 public activeExpiry;
+    uint256 public currentBoardId;
+    uint256 public ivLimit = 2 * 1e18;
     address public lyraRewardRecipient;
 
     //uint public optionSize;
@@ -49,11 +55,22 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     // ADMIN //
     ///////////
 
+    // Implementation having proxy is unsafe
+    // for setting immutable vars it's ok
+    // aside from that we are setting ownership
+    // initialize function reset ownership
     constructor(
         HackMoneyVault _vault,
         OptionType _optionType,
         GWAVOracle _gwavOracle
     ) HackMoneyStrategyBase(_vault, _optionType, _gwavOracle) {}
+
+    // Note: - We can either change immutable variable in the strategy base to no longer be immutable
+    //       - Or let them be immutable and change how implementation constructor is done
+    //
+    function initialize() external initializer {
+        _transferOwnership(_msgSender());
+    }
 
     /**
      * @dev update the strategy detail for the new round.
@@ -70,7 +87,7 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     /**
      * @dev update the iv limit
      */
-    function setIvLimit(uint _ivLimit) external onlyOwner {
+    function setIvLimit(uint256 _ivLimit) external onlyOwner {
         ivLimit = _ivLimit;
     }
 
@@ -92,7 +109,7 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      * @dev set the board id that will be traded for the next round
      * @param boardId lyra board Id.
      */
-    function setBoard(uint boardId) external onlyVault {
+    function setBoard(uint256 boardId) external onlyVault {
         Board memory board = getBoard(boardId);
         require(_isValidExpiry(board.expiry), "invalid board");
         activeExpiry = board.expiry;
@@ -118,15 +135,15 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      * @return premiumReceived
      * @return collateralToAdd
      */
-    function doTrade(uint size)
+    function doTrade(uint256 size)
         external
         onlyVault
         returns (
-            uint positionId1,
-            uint positionId2,
-            uint premiumReceived,
-            uint collateralToAdd,
-            uint premiumExchangeValue
+            uint256 positionId1,
+            uint256 positionId2,
+            uint256 premiumReceived,
+            uint256 collateralToAdd,
+            uint256 premiumExchangeValue
         )
     // uint exchangeValue
     {
@@ -144,19 +161,19 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     function _tradeOptions()
         internal
         returns (
-            uint positionId1,
-            uint positionId2,
-            uint premiumReceived,
-            uint collateralToAdd,
-            uint premiumExchangeValue
+            uint256 positionId1,
+            uint256 positionId2,
+            uint256 premiumReceived,
+            uint256 collateralToAdd,
+            uint256 premiumExchangeValue
         )
     {
         (Strike memory strike1, Strike memory strike2) = _getTradeStrikes();
 
-        uint premiumReceived1;
-        uint premiumReceived2;
-        uint collateralToAdd1;
-        uint collateralToAdd2;
+        uint256 premiumReceived1;
+        uint256 premiumReceived2;
+        uint256 collateralToAdd1;
+        uint256 collateralToAdd2;
 
         (positionId1, premiumReceived1, collateralToAdd1) = _tradeStrike(
             strike1
@@ -180,13 +197,13 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     function _tradeStrike(Strike memory strike)
         internal
         returns (
-            uint positionId,
-            uint premiumReceived,
-            uint collateralToAdd
+            uint256 positionId,
+            uint256 premiumReceived,
+            uint256 collateralToAdd
         )
     //uint setCollateralTo
     {
-        uint setCollateralTo;
+        uint256 setCollateralTo;
         (collateralToAdd, setCollateralTo) = getRequiredCollateral(strike);
 
         (positionId, premiumReceived) = _sellStrike(strike, setCollateralTo);
@@ -199,31 +216,31 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      * @return premiumReceived
      */
     function _tradePremiums(
-        uint size,
-        uint collateralToAdd1,
-        uint collateralToAdd2
+        uint256 size,
+        uint256 collateralToAdd1,
+        uint256 collateralToAdd2
     )
         internal
         returns (
-            uint positionId1,
-            uint positionId2,
-            uint premiumReceived,
-            uint exchangeValue
+            uint256 positionId1,
+            uint256 positionId2,
+            uint256 premiumReceived,
+            uint256 exchangeValue
         )
     {
         // exchange susd to seth
 
         exchangeValue = _exchangePremiums(size);
 
-        uint sellAmount = exchangeValue / 2;
+        uint256 sellAmount = exchangeValue / 2;
         (Strike memory strike1, Strike memory strike2) = _getTradeStrikes();
-        uint premiumReceived1;
+        uint256 premiumReceived1;
         (positionId1, premiumReceived1) = _sellPremiums(
             strike1,
             sellAmount,
             collateralToAdd1
         );
-        uint premiumReceived2;
+        uint256 premiumReceived2;
         (positionId2, premiumReceived2) = _sellPremiums(
             strike2,
             sellAmount,
@@ -245,15 +262,15 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     function getRequiredCollateral(Strike memory strike)
         public
         view
-        returns (uint collateralToAdd, uint setCollateralTo)
+        returns (uint256 collateralToAdd, uint256 setCollateralTo)
     {
-        uint sellAmount = strategyDetail.size;
+        uint256 sellAmount = strategyDetail.size;
         collateralToAdd = _getFullCollateral(strike.strikePrice, sellAmount);
         setCollateralTo = collateralToAdd;
 
         // get existing position info if active
-        uint existingAmount = 0;
-        uint existingCollateral = 0;
+        uint256 existingAmount = 0;
+        uint256 existingCollateral = 0;
         if (_isActiveStrike(strike.id)) {
             OptionPosition memory position = getPositions(
                 _toDynamic(strikeToPositionId[strike.id])
@@ -273,18 +290,18 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      * @return positionId
      * @return premiumReceived
      */
-    function _sellStrike(Strike memory strike, uint setCollateralTo)
+    function _sellStrike(Strike memory strike, uint256 setCollateralTo)
         internal
-        returns (uint, uint)
+        returns (uint256, uint256)
     {
         // get minimum expected premium based on minIv
-        uint minExpectedPremium = _getPremiumLimit(
+        uint256 minExpectedPremium = _getPremiumLimit(
             strike,
             strategyDetail.minVol,
             strategyDetail.size
         );
-        uint strikeId = strike.id;
-        uint initIv = strike.boardIv.multiplyDecimal(strike.skew);
+        uint256 strikeId = strike.id;
+        uint256 initIv = strike.boardIv.multiplyDecimal(strike.skew);
 
         // perform trade
         TradeResult memory result = openPosition(
@@ -296,12 +313,12 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
                 amount: strategyDetail.size,
                 setCollateralTo: setCollateralTo,
                 minTotalCost: 0,
-                maxTotalCost: type(uint).max,
+                maxTotalCost: type(uint256).max,
                 rewardRecipient: lyraRewardRecipient // set to zero address if don't want to wait for whitelist
             })
         );
         Strike memory finalStrike = getStrikes(_toDynamic(strikeId))[0];
-        uint finalIv = finalStrike.boardIv.multiplyDecimal(finalStrike.skew);
+        uint256 finalIv = finalStrike.boardIv.multiplyDecimal(finalStrike.skew);
         require(initIv - finalIv < ivLimit, "IV_LIMIT_HIT");
 
         lastTradeTimestamp[strike.id] = block.timestamp;
@@ -320,17 +337,17 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      */
     function _sellPremiums(
         Strike memory strike,
-        uint size,
-        uint collateralToAdd
-    ) internal returns (uint, uint) {
+        uint256 size,
+        uint256 collateralToAdd
+    ) internal returns (uint256, uint256) {
         // get minimum expected premium based on minIv
-        uint minExpectedPremium = _getPremiumLimit(
+        uint256 minExpectedPremium = _getPremiumLimit(
             strike,
             strategyDetail.minVol,
             size
         );
-        uint strikeId = strike.id;
-        uint initIv = strike.boardIv.multiplyDecimal(strike.skew);
+        uint256 strikeId = strike.id;
+        uint256 initIv = strike.boardIv.multiplyDecimal(strike.skew);
 
         // perform trade
         TradeResult memory result = openPosition(
@@ -342,12 +359,12 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
                 amount: size,
                 setCollateralTo: size + collateralToAdd,
                 minTotalCost: 0,
-                maxTotalCost: type(uint).max,
+                maxTotalCost: type(uint256).max,
                 rewardRecipient: lyraRewardRecipient // set to zero address if don't want to wait for whitelist
             })
         );
         Strike memory finalStrike = getStrikes(_toDynamic(strikeId))[0];
-        uint finalIv = finalStrike.boardIv.multiplyDecimal(finalStrike.skew);
+        uint256 finalIv = finalStrike.boardIv.multiplyDecimal(finalStrike.skew);
         require(initIv - finalIv < ivLimit, "IV_LIMIT_HIT");
 
         lastTradeTimestamp[strike.id] = block.timestamp;
@@ -369,31 +386,36 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
         returns (Strike memory smallStrike, Strike memory bigStrike)
     {
         // get all strike Ids for current board
-        uint[] memory strikeIds = optionMarket.getBoardStrikes(currentBoardId);
+        uint256[] memory strikeIds = optionMarket.getBoardStrikes(
+            currentBoardId
+        );
 
         // get small and big strike Ids
-        uint smallStrikeId = strikeIds[0];
-        uint bigStrikeId = strikeIds[0];
+        uint256 smallStrikeId = strikeIds[0];
+        uint256 bigStrikeId = strikeIds[0];
 
         // init strikes
         smallStrike = getStrikes(_toDynamic(smallStrikeId))[0];
         bigStrike = getStrikes(_toDynamic(bigStrikeId))[0];
 
-        (uint smallDeltaGap, ) = _getDeltaGap(smallStrike, true);
-        (uint bigDeltaGap, ) = _getDeltaGap(bigStrike, false);
+        (uint256 smallDeltaGap, ) = _getDeltaGap(smallStrike, true);
+        (uint256 bigDeltaGap, ) = _getDeltaGap(bigStrike, false);
         console.log("smallDeltaGap:", smallDeltaGap / 10**15, " / 10 %");
         console.log("bigDeltaGap:", bigDeltaGap / 10**15, "/ 10 %");
 
-        for (uint i = 1; i < strikeIds.length - 1; i++) {
+        for (uint256 i = 1; i < strikeIds.length - 1; i++) {
             // Get current Strike
-            uint currentStrikeId = strikeIds[i];
+            uint256 currentStrikeId = strikeIds[i];
             Strike memory currentStrike = getStrikes(
                 _toDynamic(currentStrikeId)
             )[0];
 
             // Get current delta gaps
-            (uint currentSmallDeltaGap, ) = _getDeltaGap(currentStrike, true);
-            (uint currentBigDeltaGap, ) = _getDeltaGap(currentStrike, false);
+            (uint256 currentSmallDeltaGap, ) = _getDeltaGap(
+                currentStrike,
+                true
+            );
+            (uint256 currentBigDeltaGap, ) = _getDeltaGap(currentStrike, false);
 
             if (currentSmallDeltaGap < smallDeltaGap) {
                 smallStrike = currentStrike;
@@ -428,10 +450,10 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
      * @param amount of options to cover
      * @return fullCollat much collateral is needed for `amount` sell of options
      */
-    function _getFullCollateral(uint strikePrice, uint amount)
+    function _getFullCollateral(uint256 strikePrice, uint256 amount)
         internal
         view
-        returns (uint fullCollat)
+        returns (uint256 fullCollat)
     {
         // calculate required collat based on collatBuffer and collatPercent
         fullCollat = _isBaseCollat()
@@ -446,15 +468,17 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     function _getDeltaGap(Strike memory strike, bool isSmallStrike)
         public
         view
-        returns (uint deltaGap, int callDelta)
+        returns (uint256 deltaGap, int256 callDelta)
     {
-        int targetDelta = isSmallStrike
+        int256 targetDelta = isSmallStrike
             ? strategyDetail.maxtargetDelta
             : strategyDetail.mintargetDelta;
-        uint[] memory strikeId = _toDynamic(strike.id);
+        uint256[] memory strikeId = _toDynamic(strike.id);
         callDelta = getDeltas(strikeId)[0];
 
-        int delta = _isCall() ? callDelta : callDelta - SignedDecimalMath.UNIT;
+        int256 delta = _isCall()
+            ? callDelta
+            : callDelta - SignedDecimalMath.UNIT;
         deltaGap = _abs(targetDelta - delta);
     }
 
@@ -465,17 +489,20 @@ contract HackMoneyStrategy is HackMoneyStrategyBase, IHackMoneyStrategy {
     /**
      * @dev check if the expiry of the board is valid according to the strategy
      */
-    function _isValidExpiry(uint expiry) public view returns (bool isValid) {
-        uint secondsToExpiry = _getSecondsToExpiry(expiry);
+    function _isValidExpiry(uint256 expiry) public view returns (bool isValid) {
+        uint256 secondsToExpiry = _getSecondsToExpiry(expiry);
         isValid = (secondsToExpiry >= strategyDetail.minTimeToExpiry &&
             secondsToExpiry <= strategyDetail.maxTimeToExpiry);
     }
 
-    function _exchangePremiums(uint size) internal returns (uint baseReceived) {
+    function _exchangePremiums(uint256 size)
+        internal
+        returns (uint256 baseReceived)
+    {
         ExchangeRateParams memory exchangeParams = getExchangeParams();
         //uint quoteBal = quoteAsset.balanceOf(address(this));
         // exchange quote asset to base asset
-        uint minQuoteExpected = size
+        uint256 minQuoteExpected = size
             .divideDecimal(exchangeParams.spotPrice)
             .multiplyDecimal(
                 DecimalMath.UNIT - exchangeParams.baseQuoteFeeRate
--- packages/contracts/test/integration-tests/strategies/hack-money-vault.ts
+++ packages/contracts/test/integration-tests/strategies/hack-money-vault.ts
@@ -1,36 +1,44 @@
-import { lyraConstants } from '@lyrafinance/protocol';
-import { MAX_UINT, OptionType, toBN, ZERO_ADDRESS } from '@lyrafinance/protocol/dist/scripts/util/web3utils';
-import { BasicFeeCounter__factory, ERC20__factory } from '@lyrafinance/protocol/dist/typechain-types';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { expect } from 'chai';
-import { ethers, network } from 'hardhat';
-import { HackMoneyVault__factory } from '../../../typechain-types';
+import { lyraConstants } from "@lyrafinance/protocol";
+import {
+  MAX_UINT,
+  OptionType,
+  toBN,
+  ZERO_ADDRESS,
+} from "@lyrafinance/protocol/dist/scripts/util/web3utils";
+import {
+  BasicFeeCounter__factory,
+  ERC20__factory,
+} from "@lyrafinance/protocol/dist/typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+import { HackMoneyVault__factory } from "../../../typechain-types";
 import {
   HackMoneyStrategyLibraryAddresses,
   HackMoneyStrategy__factory,
-} from '../../../typechain-types/factories/HackMoneyStrategy__factory';
-import { HackMoneyStrategyDetailStruct } from '../../../typechain-types/HackMoneyStrategy';
-import { targets as lyraGlobal } from './lyra-mainnet.json';
+} from "../../../typechain-types/factories/HackMoneyStrategy__factory";
+import { HackMoneyStrategyDetailStruct } from "../../../typechain-types/HackMoneyStrategy";
+import { targets as lyraGlobal } from "./lyra-mainnet.json";
 
 const lyraMarket = lyraGlobal.markets.sETH;
 
 const strategyDetail: HackMoneyStrategyDetailStruct = {
   minTimeToExpiry: lyraConstants.DAY_SEC,
   maxTimeToExpiry: lyraConstants.WEEK_SEC * 2,
-  mintargetDelta: toBN('0.15'),
-  maxtargetDelta: toBN('0.85'),
-  // maxDeltaGap: toBN("0.05"), // accept delta from 0.10~0.20 or 0.80~0.90
-  minVol: toBN('0.8'), // min vol to sell. (also used to calculate min premium for call selling vault)
-  size: toBN('100'),
+  mintargetDelta: toBN("0.15"),
+  maxtargetDelta: toBN("0.85"),
+  maxDeltaGap: toBN("0.05"), // accept delta from 0.10~0.20 or 0.80~0.90
+  minVol: toBN("0.8"), // min vol to sell. (also used to calculate min premium for call selling vault)
+  size: toBN("100"),
 };
 
-describe('Hack Money Vault integration test', async () => {
+describe("Hack Money Vault integration test", async () => {
   let deployer: SignerWithAddress;
   // let manager: SignerWithAddress;
   // let randomUser: SignerWithAddress;
   // let randomUser2: SignerWithAddress;
 
-  before('assign roles', async () => {
+  before("assign roles", async () => {
     const addresses = await ethers.getSigners();
     deployer = addresses[0];
     // manager = addresses[1];
@@ -38,131 +46,143 @@ describe('Hack Money Vault integration test', async () => {
     // randomUser2 = addresses[9];
   });
 
-  it('deploy on kovan fork, deposit and try to trade', async () => {
+  it("deploy on kovan fork, deposit and try to trade", async () => {
     await network.provider.request({
-      method: 'hardhat_reset',
+      method: "hardhat_reset",
       params: [
         {
           forking: {
             // TODO: Remove into env
-            jsonRpcUrl: 'https://opt-mainnet.g.alchemy.com/v2/dW3_J05iOi-kuyk1Zgy7bwXip_gXMeSX',
+            jsonRpcUrl:
+              "https://opt-mainnet.g.alchemy.com/v2/dW3_J05iOi-kuyk1Zgy7bwXip_gXMeSX",
             blockNumber: 13578217,
           },
         },
       ],
     });
 
-    const whaleAddress = '0xa5f7a39e55d7878bc5bd754ee5d6bd7a7662355b';
+    const whaleAddress = "0xa5f7a39e55d7878bc5bd754ee5d6bd7a7662355b";
 
     // const balance = await ethers.provider.getBalance(
     // "0xD34F2e9916473C5eFA8A255f5b8738eCd4205317"
     // );
 
     await network.provider.request({
-      method: 'hardhat_impersonateAccount',
+      method: "hardhat_impersonateAccount",
       params: [whaleAddress],
     });
 
     const whale = await ethers.getSigner(whaleAddress);
-    const sETH = ERC20__factory.connect('0xE405de8F52ba7559f9df3C368500B6E6ae6Cee49', deployer);
+    const sETH = ERC20__factory.connect(
+      "0xE405de8F52ba7559f9df3C368500B6E6ae6Cee49",
+      deployer
+    );
     // const sETH = new Contract(
     // lyraMarket.BaseAsset.address,
     // lyraMarket.BaseAsset.abi,
     // whale
     // ) as ERC20;
-    const sUSD = ERC20__factory.connect('0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9', deployer);
+    const sUSD = ERC20__factory.connect(
+      "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9",
+      deployer
+    );
     // const sUSD = new Contract(
     // lyraGlobal.QuoteAsset.address,
     // lyraGlobal.QuoteAsset.abi,
     // deployer
     // );
 
-    console.log('succesfully transferring');
-    await sETH.connect(whale).transfer(deployer.address, ethers.utils.parseEther('100'));
+    console.log("succesfully transferring");
+    await sETH
+      .connect(whale)
+      .transfer(deployer.address, ethers.utils.parseEther("100"));
 
     const HackMoneyVaultFactory = new HackMoneyVault__factory(deployer);
     const decimals = 18;
-    const cap = ethers.utils.parseEther('100000'); // 100k USD as cap
+    const cap = ethers.utils.parseEther("100000"); // 100k USD as cap
     const lyraVault = await HackMoneyVaultFactory.connect(deployer).deploy(
       sUSD.address,
       ZERO_ADDRESS,
       lyraConstants.DAY_SEC * 7,
-      'Lyra Covered Strangle Share',
-      'CSS',
+      "Lyra Covered Strangle Share",
+      "CSS",
       {
         decimals,
         cap,
         asset: sETH.address, // collateral asset
-      },
+      }
     );
 
     const linkAddresses: HackMoneyStrategyLibraryAddresses = {
-      '@lyrafinance/protocol/contracts/libraries/BlackScholes.sol:BlackScholes':
-        '0x409f9A1Ee61E94B91b11e3696DF2108EFc7C3EF5',
+      "@lyrafinance/protocol/contracts/libraries/BlackScholes.sol:BlackScholes":
+        "0x409f9A1Ee61E94B91b11e3696DF2108EFc7C3EF5",
     };
-    const LyraStrategyFactory = new HackMoneyStrategy__factory(linkAddresses, deployer);
-    console.log('pre strategy deploy');
+    const LyraStrategyFactory = new HackMoneyStrategy__factory(
+      linkAddresses,
+      deployer
+    );
+    console.log("pre strategy deploy");
     const lyraStrategy = await LyraStrategyFactory.connect(deployer).deploy(
       lyraVault.address,
-      OptionType.SHORT_CALL_BASE,
+      OptionType.SHORT_CALL_BASE
     );
 
     const BasicFeeCounterFactory = new BasicFeeCounter__factory(deployer);
     const feeCounter = await BasicFeeCounterFactory.connect(deployer).deploy();
 
-    console.log('before adapter init');
+    console.log("before adapter init");
     await lyraStrategy
       .connect(deployer)
       .initAdapter(
         lyraGlobal.LyraRegistry.address,
         lyraMarket.OptionMarket.address,
-        '0x0100fBf414071977B19fC38e6fc7c32FE444F5C9',
-        feeCounter.address,
+        "0x0100fBf414071977B19fC38e6fc7c32FE444F5C9",
+        feeCounter.address
       );
 
     await lyraVault.connect(deployer).setStrategy(lyraStrategy.address);
     await lyraStrategy.connect(deployer).setStrategyDetail(strategyDetail);
 
     let vaultBalance = await lyraVault.totalBalance();
-    expect(vaultBalance).to.equal('0');
+    expect(vaultBalance).to.equal("0");
 
-    console.log('pre deposit');
+    console.log("pre deposit");
     await sETH.connect(deployer).approve(lyraVault.address, MAX_UINT);
-    await lyraVault.deposit(ethers.utils.parseEther('20'));
+    await lyraVault.deposit(ethers.utils.parseEther("20"));
 
     vaultBalance = await lyraVault.totalBalance();
-    expect(vaultBalance).to.equal(ethers.utils.parseEther('20'));
+    expect(vaultBalance).to.equal(ethers.utils.parseEther("20"));
 
-    console.log('pre start next round');
+    console.log("pre start next round");
     const blockNumber = await ethers.provider.getBlockNumber();
     const block = await ethers.provider.getBlock(blockNumber);
     console.log(block.timestamp);
     console.log(block.number);
-    await lyraVault.startNextRound('3');
-    console.log('able to start next round');
+    await lyraVault.startNextRound("3");
+    console.log("able to start next round");
 
     vaultBalance = await lyraVault.totalBalance();
-    expect(vaultBalance).to.equal(ethers.utils.parseEther('20'));
+    expect(vaultBalance).to.equal(ethers.utils.parseEther("20"));
 
-    console.log('pre deposit 2');
-    await lyraVault.deposit(ethers.utils.parseEther('1'));
+    console.log("pre deposit 2");
+    await lyraVault.deposit(ethers.utils.parseEther("1"));
 
     vaultBalance = await lyraVault.totalBalance();
-    expect(vaultBalance).to.equal(ethers.utils.parseEther('21'));
-    console.log('pre trade');
-    await lyraVault.trade(ethers.utils.parseEther('5'), {
+    expect(vaultBalance).to.equal(ethers.utils.parseEther("21"));
+    console.log("pre trade");
+    await lyraVault.trade(ethers.utils.parseEther("5"), {
       gasLimit: 30000000,
     });
-    await lyraVault.trade(ethers.utils.parseEther('5'), {
+    await lyraVault.trade(ethers.utils.parseEther("5"), {
       gasLimit: 30000000,
     });
-    console.log('Traded');
+    console.log("Traded");
 
     // await expect(lyraVault.trade(ethers.utils.parseEther("5")))
     //   .to.emit(lyraVault, "Trade")
     //   .withArgs(deployer.address, 182, 183, 1817, 10);
 
     vaultBalance = await lyraVault.totalBalance();
-    expect(vaultBalance).to.equal(ethers.utils.parseEther('21'));
+    expect(vaultBalance).to.equal(ethers.utils.parseEther("21"));
   });
 });

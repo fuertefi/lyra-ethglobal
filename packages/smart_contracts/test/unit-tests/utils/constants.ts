753f8493ac7e8d6d378cb2abcba92e8d03ac543c
Igor Line <igor@line.ee>
2022-07-26 17:22:43 +0200

    stop tracking openzeppelin deployments and remove non used package

--- packages/smart_contracts/test/unit-tests/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const FEE_MULTIPLIER = 1e6;
-
-export const SEC_IN_YEAR = 31540000;
--- packages/smart_contracts/test/unit-tests/utils/synthetixUtils.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "hardhat";
-
-export const toBytes32 = (msg: string): string => {
-  return ethers.utils.formatBytes32String(msg);
-};
